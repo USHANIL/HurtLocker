9d3b5b77d4970e7c8bda3166022e655c0b9cfc8f
ushanil <ush.gopi@gmail.com>
2019-12-02 13:52:39 -0500

    still need to work on output print formatting

--- src/main/java/RawDataParser.java
+++ src/main/java/RawDataParser.java
@@ -1,9 +1,11 @@
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class RawDataParser {
     // i am thinking the string to be item:price and integer to be number of times it occured
-    Map<String, Integer> listOfItems = new HashMap<String, Integer>(); //todo we may have to change it
+    Map<String, Integer> listOfItems = new LinkedHashMap<String, Integer>(); //todo we may have to change it.. itemprice and count
+    Map<String, Integer> listOfItemsandPrice = new LinkedHashMap<String, Integer>(); //todo to store item and count
 
     public void parseInput(String input){
         String[] itemsArray = input.split("##"); //split items from text file separated by ##
@@ -32,6 +34,12 @@ public class RawDataParser {
 
     }
 
+    public void addItemandCount(String item){
+        int count =1;
+        if(listOfItemsandPrice.containsKey(item)){count += listOfItemsandPrice.get(item);}
+        listOfItemsandPrice.put(item,count);
+    }
+
     //split by - data will be either name or Price
     public String validateInput(String item, String price){
 
@@ -39,19 +47,36 @@ public class RawDataParser {
         String[] outputPrice = price.split("[:]");
         StringBuilder sb = new StringBuilder();
         if(outputItem.length >1 && outputPrice.length >1) {
-            sb.append(outputItem[1].toLowerCase().replace("0","o"));
+            String itemName = outputItem[1].toLowerCase().replace("0","o");
+            addItemandCount(itemName);
+
+            sb.append(itemName);
             sb.append(":");
             sb.append(outputPrice[1].toLowerCase());
         }
         return sb.toString();
     }
 
-    public void printMap(){
-       // for (int i=0; i<listOfItems.size(); i++) {
-            System.out.println(listOfItems);
+    public void printMap() {
 
-       // }
+        for (HashMap.Entry<String, Integer> entry : listOfItemsandPrice.entrySet()) {
+            String str = entry.getKey();
+            Integer num = entry.getValue();
+
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("name:\t"+str)
+                    .append("\t\tseen: "+num+" times\n")
+                    .append("=============\t\t=============\n");
+          /*  for(Map.Entry<String,Integer> entry : priceAndNumberOfPriceOccuances.entrySet()){
+
+                stringBuilder.append("Price:\t"+entry.getKey())
+                        .append("\t\tseen: "+entry.getValue()+" times\n")
+                        .append("-------------\t\t-------------\n");
+            } */
+            System.out.println(stringBuilder);
+        }
     }
-
 
 }
