4782bf94c246eaaa2da2dceec05bd9ecd569b6d9
ushanil <ush.gopi@gmail.com>
2019-12-02 11:28:16 -0500

    RawDataParser - still working

--- src/main/java/RawDataParser.java
+++ src/main/java/RawDataParser.java
@@ -8,31 +8,31 @@ public class RawDataParser {
         String[] itemsArray = input.split("##"); //split items from text file separated by ##
 
         for (int i=0; i<itemsArray.length; i++){
-            //split each item for its name and price.. not alph, not number, not : . /
+            //split each item for its name and price.. not alpha, not number, not : . /
             String[] currentItem = itemsArray[i].split("[^a-zA-Z0-9:./]");
             addItemToMap(currentItem[0], currentItem[1]);
         }
     }
 
     public void addItemToMap(String item, String price){
-        String correctItem = validateItem(item);
-        Double correctPrice = validatePrice(price);
 
-        listOfItems.put(correctItem,correctPrice);
+        String correctItem = validateInput(item);
+        Double correctPrice = Double.valueOf(validateInput(price));
+
+        if (correctItem != null)
+        {
+            listOfItems.put(correctItem, correctPrice);
+        }
+        else {
+
+        }
 
     }
 
-    public String validateItem(String item){
-        String output = null;
+    public String validateInput(String input){
+        String output = input;
         return output;
     }
-
-    public Double validatePrice(String price){
-        Double output = null;
-        return output;
-    }
-
-
 
 
 }
